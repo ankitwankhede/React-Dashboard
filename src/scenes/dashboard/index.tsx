28644e6472c46b27a9fa275290d75a7c1e7e5dc4
ankitwankhede <ankitwankhedekar@gmail.com>
2025-08-25 00:14:27 +0530

    Changes 1. removed name.

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="Dashboard" subtitle="Welcome to Ankit your dashboard" />
+        <Header title="Dashboard" subtitle="Welcome to your dashboard" />
       </Box>
     </Box>
   );
